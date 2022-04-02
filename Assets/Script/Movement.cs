ba3d184dec1f74bf1cb434678055169c1d2ac187
Rizal Herliansyah Hidayat <39128288+Rizalmultimedia2@users.noreply.github.com>
2022-04-02 10:49:34 +0700

    scene

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public int a = 10;
+    public Camera mainCamera;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Debug.Log();
+        mainCamera = Camera.main;   
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(Input.GetKey(KeyCode.W)){
+            Move(Vector2.up);
+        }else if(Input.GetKey(KeyCode.S)){
+            Move(Vector2.down);
+        }else if(Input.GetKey(KeyCode.A)){
+            Move(Vector2.left);
+        }else if(Input.GetKey(KeyCode.D)){
+            Move(Vector2.right);
+        }
+    }
+
+    private async void Move(Vector2 input){
+        Vector3 forward = mainCamera.transform.forward;
+        Vector3 right = mainCamera.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        right.Normalize();
+        forward.Normalize();
         
+        Vector3 direction = forward * input.y + right * input.x;
+
+        transform.position = transform.position + direction * a * Time.deltaTime;
+
     }
 }
