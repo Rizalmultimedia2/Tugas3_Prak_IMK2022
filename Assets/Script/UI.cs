d37ed3d908dbc78987dd9d39a2f9df9d4c36ed97
Rizal Herliansyah Hidayat <39128288+Rizalmultimedia2@users.noreply.github.com>
2022-04-02 11:05:54 +0700

    add menu scene

---
+++ Assets/Script/UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
